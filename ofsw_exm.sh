b6f9a3a6d8717fceca4343497ef7dd6513a4413f
macauley_cheng <macauley_cheng@accton.com>
2015-10-13 14:15:59 +0800

    ofsw_exm.sh

--- ofsw_exm.sh
+++ ofsw_exm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo ofdatapath -i eth1,eth2 --datapath-id=000000111111 --no-local-port punix:/home/manager/ofdp &
-sudo ofprotocol unix:/home/manager/ofdp tcp:127.0.0.1:6633
+ofdatapath -i eth1,eth2 --datapath-id=000000111111 --no-local-port punix:/home/manager/ofdp &
+ofprotocol unix:/home/manager/ofdp tcp:127.0.0.1:6633
